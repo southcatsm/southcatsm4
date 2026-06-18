6bd261cf95e2e5e3cf22bd4944e4881a770d26af
Northrabbit Saltluo <141730585+northrabsl@users.noreply.github.com>
2026-06-18 23:26:54 +0800

    upd commentConfig.ts
    an api has been modified.

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
-		envId: "https://api-comment.southcat.cc",
+		envId: "https://api-rvs-twikoo.southcat.cc",
 		lang: SITE_LANG,
 	},
 	giscus: {
